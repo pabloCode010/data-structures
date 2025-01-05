527abff2a29ef9e82de85ed89fdc455a5c507c7f
Pablo <pablocode010@gmail.com>
2025-01-04 19:14:25 -0600

    Add data about two dimensional arrays

---
+++ 1-arrays/3-two-dimensional/ejemplo-array-3x3-lineal.go
@@ -0,0 +1,24 @@
+package main
+
+// func getPosition(columns, i, j int) int {
+// 	return columns*i + j
+// }
+
+// func main() {
+// 	var array [9]int
+// 	var columns = 3
+
+// 	for i := 0; i < 3; i++ {
+// 		for j := 0; j < 3; j++ {
+// 			pos := getPosition(columns, i, j)
+// 			array[pos] = pos + 1
+// 		}
+// 	}
+
+// 	for i := 0; i < 3; i++ {
+// 		for j := 0; j < 3; j++ {
+// 			fmt.Print(array[getPosition(columns, i, j)], " ")
+// 		}
+// 		fmt.Println()
+// 	}
+// }
---
+++ 1-arrays/3-two-dimensional/example-array-3x3.go
@@ -0,0 +1,22 @@
+package main
+
+// func main() {
+// 	var array [3][3]int
+//        i  j
+// 	array[0][0] = 1
+// 	array[0][1] = 2
+// 	array[0][2] = 3
+// 	array[1][0] = 4
+// 	array[1][1] = 5
+// 	array[1][2] = 6
+// 	array[2][0] = 7
+// 	array[2][1] = 8
+// 	array[2][2] = 9
+
+// 	for i := 0; i < 3; i++ {
+// 		for j := 0; j < 3; j++ {
+// 			fmt.Print(array[i][j], " ")
+// 		}
+// 		fmt.Println()
+// 	}
+// }
